6e8f8de25ac3eaac7786fe35451d99af1f9e145f
azat <arykov.azat.kg@gmail.com>
2023-01-09 20:09:10 +0600

    add new component todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import TodoList from './components/todoList/TodoList';
+import { useState } from 'react';
 
 function App() {
+  let [plus, setPlus] = useState(0);
+  let [inputText, setInputText] = useState('hello');
+
+  const listsItem = [
+    { id: 1, text: 'выучить базу js' },
+    { id: 2, text: 'выучить базу html' },
+    { id: 3, text: 'выучить базу css' },
+    { id: 4, text: 'выучить базу vue' },
+    { id: 5, text: 'выучить базу vue' },
+  ];
+
+  // const onAdd = () => {
+  //   setPlus(plus + 1)
+  // }
+  const onMinus = () => {
+    setPlus(plus - 1);
+  };
+  const onInputChange = (event) => {
+    setInputText(event.target.value);
+    console.log(event);
+  };
   return (
     <div>
-    
+      <h1>{inputText}</h1>
+      <input type="text" value={inputText} onChange={onInputChange} />
+      <h1>{plus}</h1>
+      <button onClick={() => setPlus(plus + 1)}> + increment</button>
+      <button onClick={onMinus}> - decrement</button>
+      {listsItem.map((element) => (
+        <TodoList key={element.id} element={element} />
+      ))}
+      {/* <TodoList id={1}/> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // React.createElement('button', onClick => console.log('click'), 'click' )
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
